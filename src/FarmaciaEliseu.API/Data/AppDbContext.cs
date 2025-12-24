2d33809df5ec6130823cfcdbc0115d0366c5d9bb
Hugo Novo Gouvea <97973663+Hugo-Novo-Gouvea@users.noreply.github.com>
2025-12-24 15:44:54 -0300

    fix: Alterado para que utilize o id correspondendo de cada tabela

--- src/FarmaciaEliseu.API/Data/AppDbContext.cs
+++ src/FarmaciaEliseu.API/Data/AppDbContext.cs
@@ -1,5 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using FarmaciaEliseu.API.Domain.Entities; // Importante para achar a classe Cliente
+using FarmaciaEliseu.API.Domain.Entities;
 
 namespace FarmaciaEliseu.API.Data;
 
@@ -7,15 +7,35 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
-        // Configuração para o Postgres aceitar datas antigas sem dar erro de fuso horário
         AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
     }
 
-    // Aqui é onde a mágica acontece.
-    // Estamos dizendo: "Banco, crie uma representação da tabela Clientes baseada na classe Cliente"
     public DbSet<Cliente> Clientes { get; set; }
     public DbSet<Funcionario> Funcionarios { get; set; }
     public DbSet<Produto> Produtos { get; set; }
     public DbSet<Movimento> Movimentos { get; set; }
-    
+
+    // --- ADICIONE ESTE BLOCO NO FINAL ---
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Mapeamento manual dos IDs (já que no banco não é padronizado como "id")
+        
+        modelBuilder.Entity<Cliente>()
+            .Property(c => c.Id)
+            .HasColumnName("clientes_id");
+
+        modelBuilder.Entity<Funcionario>()
+            .Property(f => f.Id)
+            .HasColumnName("funcionarios_id");
+
+        modelBuilder.Entity<Produto>()
+            .Property(p => p.Id)
+            .HasColumnName("produtos_id");
+
+        modelBuilder.Entity<Movimento>()
+            .Property(m => m.Id)
+            .HasColumnName("movimentos_id");
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
--- src/FarmaciaEliseu.API/Domain/Entities/BaseEntity.cs
+++ src/FarmaciaEliseu.API/Domain/Entities/BaseEntity.cs
@@ -3,12 +3,9 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FarmaciaEliseu.API.Domain.Entities;
 
-// "abstract" significa que você nunca vai usar "new BaseEntity()". 
-// Ela serve só de herança para os outros.
 public abstract class BaseEntity
 {
-    [Key] // Avisa que essa é a Chave Primária (PK)
-    [Column("id")] // Mapeia para a coluna "id" no Postgres (que criamos com SERIAL)
+    [Key]
     public int Id { get; set; }
 
     [Column("data_cadastro")]
